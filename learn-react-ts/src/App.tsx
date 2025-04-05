ae3cb4f66aee00a90e2b8ed89c7878c979870845
BrianXYZ <brianchima22@gmail.com>
2025-04-05 10:25:54 +0100

    updated ts config

--- learn-react-ts/src/App.tsx
+++ learn-react-ts/src/App.tsx
@@ -5,14 +5,126 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  interface User {
+    name: string
+    readonly id: number
+    email: string
+    comments: [number, string]
+  }
 
+  interface TakePhoto {
+    burst: number
+    potraitMode: boolean
+  }
+  
+  interface IgStory {
+    createStory(): void
+  }
+
+  class InstagramPost implements TakePhoto{
+    public burst: number = 1
+    constructor(
+      public potraitMode: boolean
+    ){}
+  }
+
+  class InstagramStory implements TakePhoto, IgStory{
+    createStory(): void{
+      console.log("new Story");
+      return
+    }
+    constructor(
+      public burst: number,
+      public potraitMode: boolean
+    ){}
+  }
+
+  interface User {
+    userRole: "user" | "admin" | "c-level"
+  }
+
+
+  const user1: User = {
+    name: "Brian",
+    id: 1,
+    email: "b@gmail.com",
+    comments: [1, "Hello"],
+    userRole: "c-level"
+  }
+
+  interface Arr {
+    [index: number]: unknown
+  }
+  interface Obj {
+    [key: string]: unknown
+  }
+
+  const randArr: Arr = ["string", 1]
+
+  const randObj: Obj = {
+    user: "david",
+    id: 1,
+    isAdmin: false
+  }
+  // console.log(user1);
+  console.log(typeof randArr);
+  
+  let users: User[] = [user1];
+
+  // class Food {
+  //   private readonly id: number = 1
+  //   name: string
+  //   rating: number
+  //   constructor(
+  //     name: string, rating: number
+  //   ){
+  //     this.name = name,
+  //     this.rating = rating
+  //   }
+  // }
+
+  // OR
+  // Shortcut
+
+  class Food {
+    protected _id: number = 1
+    constructor(
+      public readonly name: string,
+      public rating: number ,
+    ){}
+    
+    get getFoodName(): number {
+      return this._id;      
+    }
+
+    
+    public set id(newValue : number) {
+      if(newValue <= 1) return;
+      this._id = newValue;
+    }
+    
+  }
+  
+
+  class OtherFood extends Food {
+    private isNew: boolean = true;
+      setFood(){
+        this._id = 3
+      }
+  }
+
+  const newFood = new Food("Rice", 3);
+  // console.log(users);
+
+  // console.log(newFood);
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" rel="noopener noreferrer" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" rel="noopener noreferrer" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
